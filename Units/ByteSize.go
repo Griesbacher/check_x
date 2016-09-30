95f06060fbd7971be7b5844f57717049bd40145d
Philip Griesbacher <philip@griesbacher.org>
2016-09-30 15:46:41 +0200

    formated

--- Units/ByteSize.go
+++ Units/ByteSize.go
@@ -9,7 +9,7 @@ import (
 type ByteSize float64
 
 const (
-	_ = iota // ignore first value by assigning to blank identifier
+	_            = iota // ignore first value by assigning to blank identifier
 	kiB ByteSize = 1 << (10 * iota)
 	miB
 	giB
@@ -27,21 +27,21 @@ func castTString(b ByteSize) string {
 func (b ByteSize) String() string {
 	switch {
 	case b >= yiB:
-		return fmt.Sprintf("%sYiB", castTString(b / yiB))
+		return fmt.Sprintf("%sYiB", castTString(b/yiB))
 	case b >= ziB:
-		return fmt.Sprintf("%sZiB", castTString(b / ziB))
+		return fmt.Sprintf("%sZiB", castTString(b/ziB))
 	case b >= eiB:
-		return fmt.Sprintf("%sEiB", castTString(b / eiB))
+		return fmt.Sprintf("%sEiB", castTString(b/eiB))
 	case b >= piB:
-		return fmt.Sprintf("%sPiB", castTString(b / piB))
+		return fmt.Sprintf("%sPiB", castTString(b/piB))
 	case b >= tiB:
-		return fmt.Sprintf("%sTiB", castTString(b / tiB))
+		return fmt.Sprintf("%sTiB", castTString(b/tiB))
 	case b >= giB:
-		return fmt.Sprintf("%sGiB", castTString(b / giB))
+		return fmt.Sprintf("%sGiB", castTString(b/giB))
 	case b >= miB:
-		return fmt.Sprintf("%sMiB", castTString(b / miB))
+		return fmt.Sprintf("%sMiB", castTString(b/miB))
 	case b >= kiB:
-		return fmt.Sprintf("%sKiB", castTString(b / kiB))
+		return fmt.Sprintf("%sKiB", castTString(b/kiB))
 	}
 	return fmt.Sprintf("%sB", castTString(b))
 }
